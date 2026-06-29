e3a6aa41db45e973aaa8e559e648c28a3ac748fc
Retesh Kumar Pandey <reteshp751@gmail.com>
2026-06-29 10:08:55 +0530

    initial commit

---
+++ demo.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class demo {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ENTER A NUMBER: ");
+        int x = sc.nextInt();
+        System.out.println("square root = ");
+        System.out.println(Math.sqrt(x));
+
+    }
+}
